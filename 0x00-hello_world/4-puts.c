2cff5f080f0fa6d9feb2d70127aeed3eaa9fe8d0
chiuzo <chizomauzoma@gmail.com>
2022-09-08 12:01:21 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,20 +3,14 @@
  *
  *  * main - Entry point
  *
- *   * 
+ *   *
  *
- *    * Description: 'the program's description'
+ *    * Return: Always 0 (Success)
  *
- *     * 
- *
- *      * Return: Always 0 (Success)
- *
- *       */
+ *     */
 int main(void)
-
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
+
 }
