59baea24de3906a41a3bc39c4d2b6b2920fad0f9
chiuzo <chizomauzoma@gmail.com>
2022-09-08 11:57:39 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,18 @@
 #include<stdio.h>
-
-int main()
+/**
+ *
+ *  * main - Entry point
+ *
+ *   * 
+ *
+ *    * Description: 'the program's description'
+ *
+ *     * 
+ *
+ *      * Return: Always 0 (Success)
+ *
+ *       */
+int main(void)
 
 {
 
