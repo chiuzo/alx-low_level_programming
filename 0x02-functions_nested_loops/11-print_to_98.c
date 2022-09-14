9e74e38833d10f2e0bd909b6e2d5a2d0175fbb0e
chiuzo <chizomauzoma@gmail.com>
2022-09-14 06:36:00 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,14 +15,11 @@ void print_to_98(int n)
 		for (i = n; i <= 98; i++)
 		{
 			if (i != 98)
-			{
 				printf("%d, ", i);
 			else if (i == 98)
 				printf("%d\n", i);
-			}
 		}
-	}
-	else if (n >= 98)
+	} else if (n >= 98)
 	{
 		for (j = n; j >= 98; j--)
 		{
@@ -31,6 +28,5 @@ void print_to_98(int n)
 			else if (j == 98)
 				printf("%d\n", j);
 		}
-		
 	}
 }
