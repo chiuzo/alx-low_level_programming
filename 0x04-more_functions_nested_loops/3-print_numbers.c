402c512a8b35fa115505c2264725c459c7cb1c10
chiuzo <chizomauzoma@gmail.com>
2022-09-15 21:23:22 -0700

    #include "main.h"
    /**
    
     * print_numbers - prints the numbers, from 0 to 9,
    
     * followed by a new line
    
     */
    
    void print_numbers(void)
    
    {
    
    int i;
    
    for (i = 0; i < 10; i++)
    
    {
    
    _putchar(i + 0);
    
    }
    
    _putchar(n);
    
    }

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,7 +4,6 @@
  * print_numbers - prints the numbers, from 0 to 9,
  * followed by a new line
  */
-
 void print_numbers(void)
 {
 	int i;
@@ -13,5 +12,6 @@ void print_numbers(void)
 	{
 		_putchar(i + '0');
 	}
+
 	_putchar('\n');
 }
