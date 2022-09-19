b5f7f33facfd4932a750f0766978e93a9f0ddac9
chiuzo <chizomauzoma@gmail.com>
2022-09-19 11:42:20 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,7 +12,7 @@ void print_number(int n)
 	if (n < 0)
 	{
 		n1 = -n;
-		putchar('-');
+		_putchar('-');
 	} else
 	{
 		n1 = n;
@@ -23,5 +23,5 @@ void print_number(int n)
 		print_number(n1 / 10);
 	}
 
-	putchar((n1 % 10) + '0');
+	_putchar((n1 % 10) + '0');
 }
